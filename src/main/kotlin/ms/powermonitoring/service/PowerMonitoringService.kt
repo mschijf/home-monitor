cc4e93074b0460f136bf51b620178e981a70354d
Martin Schijf <martinschijf@bol.com>
2024-07-21 10:23:48 +0200

    adding hour and day schedules + refactoring

--- src/main/kotlin/ms/powermonitoring/service/PowerMonitoringService.kt
+++ src/main/kotlin/ms/powermonitoring/service/PowerMonitoringService.kt
@@ -1,36 +1,53 @@
 package ms.powermonitoring.service
 
+import ms.powermonitoring.homewizard.model.HomeWizardMeasurementData
 import ms.powermonitoring.homewizard.rest.HomeWizard
+import org.slf4j.LoggerFactory
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
+import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @Service
 class PowerMonitoringService(
     private val homeWizard: HomeWizard,
-    environment: Environment) {
+    environment: Environment
+) {
 
-    private val enabled = environment.getProperty("low-level.csv.enabled")?.toBoolean() ?: false
+    private val fileNameMostDetailed = environment.getProperty("most-detailed.output-file-name") ?: "defaultMostDetailed.csv"
+    private val fileNameHour = environment.getProperty("hour.output-file-name") ?: "defaultHour.csv"
+    private val fileNameDay = environment.getProperty("day.output-file-name") ?: "defaultDay.csv"
+    private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
-    fun storeLatestPowerMeasurement() {
+    fun storeMostDetailedPowerMeasurement() {
+        File(fileNameMostDetailed)
+            .appendText(homeWizard.getHomeWizardData().toCSV(includingActivePower = true))
+    }
 
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-        val current = LocalDateTime.now().format(formatter)
+    fun storeHourPowerMeasurement() {
+        File(fileNameHour)
+            .appendText(homeWizard.getHomeWizardData().toCSV(includingActivePower = false))
+    }
+    fun storeDayPowerMeasurement() {
+        File(fileNameDay)
+            .appendText(homeWizard.getHomeWizardData().toCSV(includingActivePower = false))
+    }
 
-        val response = homeWizard.getHomeWizardData()
 
-        if (enabled) {
-            print("$current;")
-            print("${response.totalPowerImportKwh};")
-            print("${response.totalPowerImportT1Kwh};")
-            print("${response.totalPowerImportT2Kwh};")
-            print("${response.activePowerWatt};")
-            print("${response.activePowerL1Watt};")
-            print("${response.activePowerL2Watt};")
-            print("${response.activePowerL3Watt};")
-        }
-
-        println()
+    private fun HomeWizardMeasurementData.toCSV(includingActivePower: Boolean): String {
+        val response = this
+        var result =  LocalDateTime.now().format(formatter) +
+                "${response.totalPowerImportKwh};" +
+                "${response.totalPowerImportT1Kwh};" +
+                "${response.totalPowerImportT2Kwh};"
+                if (includingActivePower) {
+                    result += "${response.activePowerWatt};" +
+                            "${response.activePowerL1Watt};" +
+                            "${response.activePowerL2Watt};" +
+                            "${response.activePowerL3Watt};"
+                }
+                result += "\n"
+        return result
     }
 }
--- src/main/kotlin/ms/powermonitoring/service/Scheduler.kt
+++ src/main/kotlin/ms/powermonitoring/service/Scheduler.kt
@@ -1,16 +1,47 @@
 package ms.powermonitoring.service
 
+import org.slf4j.LoggerFactory
+import org.springframework.core.env.Environment
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
-const val delay = 10000L //env.getProperty("low-level.scheduler.frequency-in-milliseconds")?.toLong() ?: 1000L
+const val delay = 10000L //env.getProperty("output.most-detailed.fetch-rate-in-milliseconds")?.toLong() ?: 1000L
 
 @Service
 class Scheduler(
-    private val service: PowerMonitoringService) {
+    private val service: PowerMonitoringService,
+    environment: Environment) {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val enabledMostDetailed = environment.getProperty("output.most-detailed.enabled")?.toBoolean() ?: false
+    private val enabledHour = environment.getProperty("output.hour.enabled")?.toBoolean() ?: false
+    private val enabledDay = environment.getProperty("output.day.enabled")?.toBoolean() ?: false
+
+    init {
+        log.info("Most detailed level measuring ${if (enabledMostDetailed) "enabled for " + delay + " ms" else "disabled"} ")
+        log.info("Hour level measuring ${if (enabledHour) "enabled" else "disabled"}")
+        log.info("Day level measuring ${if (enabledDay) "enabled" else "disabled"}")
+    }
 
     @Scheduled(fixedRate = delay)
-    fun run() {
-        service.storeLatestPowerMeasurement()
+    fun runMostDetailed() {
+        if (enabledMostDetailed) {
+            service.storeMostDetailedPowerMeasurement()
+        }
     }
+
+    @Scheduled(cron = "0 0 * * * *")
+    fun runHour() {
+        if (enabledHour) {
+            service.storeHourPowerMeasurement()
+        }
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    fun runDay() {
+        if (enabledDay) {
+            service.storeDayPowerMeasurement()
+        }
+    }
+
 }
