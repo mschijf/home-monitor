9b46a83ca986664057b8263485749561c2923cf0
Martin Schijf <martinschijf@bol.com>
2024-07-27 13:20:49 +0200

    - hour and day measurements not necessary - removed code corresponding to that

--- src/main/kotlin/ms/powermonitoring/service/MicroMeterMeasurement.kt
+++ src/main/kotlin/ms/powermonitoring/service/MicroMeterMeasurement.kt
@@ -3,18 +3,12 @@ package ms.powermonitoring.service
 import io.micrometer.core.instrument.MeterRegistry
 import ms.powermonitoring.homewizard.model.HomeWizardMeasurementData
 import org.springframework.stereotype.Service
-import java.math.BigDecimal
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.roundToInt
 
 @Service
 class MicroMeterMeasurement(
     meterRegistry: MeterRegistry) {
-
-    private var homewizardPowerT1ConsumedHourKwh = meterRegistry.gauge("homewizardPowerT1ConsumedHourKwh", AtomicInteger(0))!!
-    private var homewizardPowerT2ConsumedHourKwh = meterRegistry.gauge("homewizardPowerT2ConsumedHourKwh", AtomicInteger(0))!!
-    private var homewizardPowerT1ConsumedDayKwh = meterRegistry.gauge("homewizardPowerT1ConsumedDayKwh", AtomicInteger(0))!!
-    private var homewizardPowerT2ConsumedDayKwh = meterRegistry.gauge("homewizardPowerT2ConsumedDayKwh", AtomicInteger(0))!!
 
     private var homewizardPowerImportT1Kwh = meterRegistry.gauge("homewizardPowerImportT1", AtomicInteger(0))!!
     private var homewizardPowerImportT2Kwh = meterRegistry.gauge("homewizardPowerImportT2", AtomicInteger(0))!!
@@ -30,19 +24,4 @@ class MicroMeterMeasurement(
         homewizardActivePowerL2Watt.set(lastMeasurement.activePowerL2Watt.toDouble().roundToInt())
         homewizardActivePowerL3Watt.set(lastMeasurement.activePowerL3Watt.toDouble().roundToInt())
     }
-
-    fun setHourMetric(current: HomeWizardMeasurementData, previous: HomeWizardMeasurementData) {
-        homewizardPowerT1ConsumedHourKwh.set(current.totalPowerImportT1Kwh.diffInInt(previous.totalPowerImportT1Kwh))
-        homewizardPowerT2ConsumedHourKwh.set(current.totalPowerImportT1Kwh.diffInInt(previous.totalPowerImportT1Kwh))
-    }
-
-    fun setDayMetric(current: HomeWizardMeasurementData, previous: HomeWizardMeasurementData) {
-        homewizardPowerT1ConsumedDayKwh.set(current.totalPowerImportT1Kwh.diffInInt(previous.totalPowerImportT1Kwh))
-        homewizardPowerT2ConsumedDayKwh.set(current.totalPowerImportT1Kwh.diffInInt(previous.totalPowerImportT1Kwh))
-    }
-
-    private fun BigDecimal.diffInInt(other: BigDecimal):Int {
-        return (this.toDouble() - other.toDouble()).roundToInt()
-    }
-
 }
--- src/main/kotlin/ms/powermonitoring/service/PowerMonitoringService.kt
+++ src/main/kotlin/ms/powermonitoring/service/PowerMonitoringService.kt
@@ -14,32 +14,22 @@ class PowerMonitoringService(
     private val measurement: MicroMeterMeasurement
 ) {
 
-    private var lastDetailedMeasurement: HomeWizardMeasurementData = homeWizard.getHomeWizardData()
-
     @Scheduled(fixedRate = 10_000)
     fun detailedPowerMeasurement() {
         val homeWizardData = homeWizard.getHomeWizardData()
         repository.storeDetailedMeasurement(homeWizardData)
         measurement.setMetrics(homeWizardData)
-        lastDetailedMeasurement = homeWizardData
     }
 
     @Scheduled(cron = "0 0 * * * *")
     fun hourPowerMeasurement() {
-        val lastMeasurement = repository.retrieveLastHourlyMeasurementOrNull() ?: lastDetailedMeasurement
         val homeWizardData = homeWizard.getHomeWizardData()
         repository.storeHourlyMeasurement(homeWizardData)
-
-        if (Duration.between(lastMeasurement.time, homeWizardData.time).toSeconds() <= 3600)
-            measurement.setHourMetric(homeWizardData, lastMeasurement)
     }
 
     @Scheduled(cron = "0 0 0 * * *")
     fun dayPowerMeasurement() {
-        val lastMeasurement = repository.retrieveLastDailyMeasurementOrNull() ?: lastDetailedMeasurement
         val homeWizardData = homeWizard.getHomeWizardData()
         repository.storeDailyMeasurement(homeWizardData)
-        if (Duration.between(lastMeasurement.time, homeWizardData.time).toSeconds() <= 3600*24)
-            measurement.setDayMetric(homeWizardData, lastMeasurement)
     }
 }
