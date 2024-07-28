c6d20f3d6f59369915b8aea372207385524a5eba
Martin Schijf <martinschijf@bol.com>
2024-07-28 07:27:10 +0200

    adding (remote-on-raspberry-pi) scripts to git-repo

---
+++ script/remote/start.sh
@@ -0,0 +1,6 @@
+program="home-monitoring"
+port="8080"
+cd ~/home-monitor
+bash ./script/stop.sh
+program_version=`ls -1 $program*.jar | tail -1`
+java -Dspring.profiles.active=prod -Dserver.port=$port -jar $program_version >> ./log/$program-log 2> ./log/$program-log &
---
+++ script/remote/stop.sh
@@ -0,0 +1,3 @@
+program=home-monitoring
+kill -9 `ps x | grep java | grep $program | awk '{print $1}'` 2> /dev/null
+
