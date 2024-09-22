37bbab7825cea961fb7ac98299f596d59297b07f
Martin Schijf <martinschijf@bol.com>
2024-09-22 17:30:38 +0200

    bugfix in parsing log line

--- src/main/kotlin/ms/homemonitor/service/LogService.kt
+++ src/main/kotlin/ms/homemonitor/service/LogService.kt
@@ -46,18 +46,23 @@ data class LogLine(
     val all: String
 ) {
 
+//    2024-09-22T16:59:48.423+02:00  WARN 539802 --- [home-monitoring] [nio-8080-exec-9] .w.s.m.s.DefaultHandlerExceptionResolver : Resolved [org.springframework.web.context.request.async.AsyncRequestNotUsableException: ServletOutputStream failed to write: java.io.IOException: Broken pipe]
+//    2024-09-22T17:03:31.386+02:00  INFO 998 --- [home-monitoring] [           main] m.homemonitor.HomeMonitorApplicationKt   : Starting HomeMonitorApplicationKt v4.7.0 using Java 21.0.4 with PID 998 (/home/martinschijf/home-monitor/home-monitor-4.7.0.jar started by martinschijf in /home/martinschijf/home-monitor)
+
     companion object {
         fun of(logLine: String): LogLine {
+            val firstPart = logLine.substringBefore(" --- ")
+            val lastPart = logLine.substringAfter(" --- ")
             try {
                 return LogLine(
-                    time = stringToLocalDateTime(logLine.substring(0, 29)),
-                    type = logLine.substring(30, 35).trim(),
-                    pid = logLine.substring(36, 43).trim().toInt(),
-                    application = logLine.substring(48, 63).trim(),
-                    process = logLine.substring(66, 81).trim(),
-                    className = logLine.substring(83, 124).substringAfterLast(".").trim(),
-                    msg = logLine.substring(126).substringBefore('\n').trim(),
-                    throwable = if (logLine.contains('\n')) logLine.substringAfter('\n').trim() else "",
+                    time = stringToLocalDateTime(firstPart.substring(0, 29)),
+                    type = firstPart.substring(30, 35).trim(),
+                    pid = firstPart.substring(36).trim().toInt(),
+                    application = lastPart.substring(1, 16).trim(),
+                    process = lastPart.substring(19, 34).trim(),
+                    className = lastPart.substring(36, 77).substringAfterLast(".").trim(),
+                    msg = lastPart.substring(79).substringBefore('\n').trim(),
+                    throwable = if (lastPart.contains('\n')) lastPart.substringAfter('\n').trim() else "",
                     all = logLine
                 )
             } catch (e: Exception) {
