6d99594d70d2b4677334af36ae007ac4c9515e7e
Martin Schijf <martinschijf@bol.com>
2024-11-03 17:27:00 +0100

    5.2.0 Removed csv repository from eneco

--- src/main/kotlin/ms/homemonitor/domain/eneco/EnecoUpdateService.kt
+++ src/main/kotlin/ms/homemonitor/domain/eneco/EnecoUpdateService.kt
@@ -3,7 +3,6 @@ package ms.homemonitor.domain.eneco
 import jakarta.transaction.Transactional
 import ms.homemonitor.domain.eneco.model.EnecoConsumption
 import ms.homemonitor.domain.eneco.rest.Eneco
-import ms.homemonitor.domain.eneco.repository.EnecoRepository
 import ms.homemonitor.repository.HeathEntity
 import ms.homemonitor.repository.HeathRepository
 import org.slf4j.LoggerFactory
@@ -16,7 +15,6 @@ import java.time.LocalTime
 @Service
 class EnecoUpdateService(
     private val eneco: Eneco,
-    private val enecoRepository: EnecoRepository,
     private val heathRepository: HeathRepository
 ) {
 
@@ -24,21 +22,6 @@ class EnecoUpdateService(
 
     @Transactional
     fun updateEnecoStatistics(source: String): List<EnecoConsumption> {
-        val consumptionList = enecoRepository.readAll().distinctBy { it.date }.sortedBy { it.date }
-        val fromDate = consumptionList.lastOrNull()?.date ?: eneco.initialDate
-        val freshDataList = getNewDataBySource(source,fromDate.toLocalDate()).sortedBy { it.date }
-
-        val newList = consumptionList.filter { it.date.toLocalDate() != fromDate.toLocalDate() } + freshDataList
-
-        if (newList.distinctBy { it.date }.size != newList.size) {
-            log.warn("Eneco statistics has duplicate dates")
-        }
-        enecoRepository.storeEnecoData(newList.distinctBy { it.date })
-
-        return updateHeath(source)
-    }
-
-    fun updateHeath(source: String): List<EnecoConsumption> {
         val lastRecord = heathRepository.getLastHeathEntity()
         val beginningOfLastDay = LocalDateTime.of(lastRecord.time.toLocalDate(), LocalTime.MIDNIGHT)
         heathRepository.deleteHeathEntitiesByTimeGreaterThanEqual(beginningOfLastDay)
@@ -55,9 +38,6 @@ class EnecoUpdateService(
 
         return freshDataList
     }
-
-
-
 
     private fun getNewDataBySource(source: String, fromDate: LocalDate): List<EnecoConsumption> {
         val now = LocalDate.now()
