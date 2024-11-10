bd11bb8a0f0861af35bcbd26e58f40fd4774cd18
Martin Schijf <martinschijf@bol.com>
2024-11-10 10:33:21 +0100

    5.3.2 renaming and restructuring database tables - final version. Removed csv files, cleaned up backup scripts, stored grafana dashbaord changes

--- script/remote/backup.sh
+++ script/remote/backup.sh
@@ -2,9 +2,7 @@ backuptime=$(date +%Y%m%d_%H%M%S)
 echo starting backup at $backuptime
 
 cd ~/home-monitor/backup
-tar -czf backup_data.tar.gz ../data/
 tar -czf backup_prom.tar.gz /var/lib/prometheus/
-~/dropbox_uploader.sh upload ~/home-monitor/backup/backup_data.tar.gz Backup/home-monitor/${backuptime}_data
 ~/dropbox_uploader.sh upload ~/home-monitor/backup/backup_prom.tar.gz Backup/home-monitor/${backuptime}_prometheus
 
 echo clean up old backup files
