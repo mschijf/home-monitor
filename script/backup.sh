59426ca709f983afbec5acb70f8c4d9179ef07cf
Martin Schijf <martinschijf@bol.com>
2024-11-27 07:48:04 +0100

    6.0.0 update backup  script and install notes

--- script/backup.sh
+++ script/backup.sh
@@ -1,14 +1,11 @@
 backuptime=$(date +%Y%m%d_%H%M%S)
-echo starting backup at $backuptime
 
 cd ~/home-monitor/backup
-docker exec -i home-monitor /usr/bin/pg_dump -U home-monitor-app home-monitor > backup_postgres.sql
+docker exec -i home-monitor /usr/local/bin/pg_dump -U home-monitor-app home-monitor > backup_postgres.sql
 tar -czf backup_postgres.tar.gz ./backup_postgres.sql
 rm backup_postgres.sql
 
-
+echo -n $(date '+%Y-%m-%d %H:%M:%S')  
 ~/dropbox_uploader.sh upload ~/home-monitor/backup/backup_postgres.tar.gz Backup/home-monitor/${backuptime}_postgres
-echo clean up old backup files
 ~/home-monitor/script/cleanup_backup.sh _postgres
 
-echo ================================
