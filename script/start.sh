7c27428867b100920165588a52f0a737f0c38170
Martin Schijf <martinschijf@bol.com>
2025-02-05 07:51:54 +0100

    removing script directory from repository --> has now its own repository and is only necessary on the pi

--- script/start.sh
+++
@@ -1,8 +0,0 @@
-program="home-monitor"
-path="home-monitor"
-port="8080"
-
-cd ~/$path
-bash ./script/stop.sh
-program_version=`ls -1 $program*.jar | tail -1`
-java -Dspring.profiles.active=prod -Dserver.port=$port -jar $program_version >> ./log/$program-log 2>> ./log/$program-log &
