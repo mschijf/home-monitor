96b314484d21d3b5c766f5970897deccf6cb3dcd
mschijf <mschijf@gmail.com>
2025-11-14 08:11:08 +0100

    10.0.8: - bug fix materialized view

---
+++ src/main/resources/db/migration/V10_0_8__add_detailed_hour_energy_materialized_view_bugfix.sql
@@ -0,0 +1,42 @@
+DROP MATERIALIZED VIEW electricity_hour_detail;
+
+CREATE MATERIALIZED VIEW electricity_hour_detail AS
+with hour_standings as
+    (
+        select date_trunc('hour', time) as hour, 1000*max(power_normal_kwh + power_offpeak_kwh) as power_wh
+        from electricity
+        group by hour
+    ),
+    all_totals as
+        (
+            select hour, power_wh - LAG(power_wh) OVER (order by hour) as delta_wh
+            from hour_standings
+        )
+   , known_per_hour as
+    (
+        select date_trunc('hour', time) as hour,
+               name,
+               sum(delta_wh)           as delta_wh
+        from smart_plug
+        where (name != 'Marantz Versterker')
+        group by hour, name
+    )
+   , smart_totals as
+    (
+        select date_trunc('hour', time) as hour,
+               sum(delta_wh) as delta_wh
+        from smart_plug
+        where (name != 'Marantz Versterker')
+        group by hour
+    )
+   , unknown_per_hour as
+    (
+        select all_totals.hour,
+               'Unknown' as name,
+               (all_totals.delta_wh - coalesce(smart_totals.delta_wh,0)) as delta_wh
+        from all_totals left join smart_totals on all_totals.hour = smart_totals.hour
+    )
+
+    (select hour as time, name, delta_wh from known_per_hour
+     union
+     select hour as time, name, delta_wh from unknown_per_hour)
