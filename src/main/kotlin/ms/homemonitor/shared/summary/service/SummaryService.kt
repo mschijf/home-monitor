6a170279c03fa8a1added1cd5e869f631db9730f
mschijf <mschijf@gmail.com>
2026-01-02 06:46:11 +0100

    10.0.9: - refactoring summary based on last hour

--- src/main/kotlin/ms/homemonitor/shared/summary/service/SummaryService.kt
+++ src/main/kotlin/ms/homemonitor/shared/summary/service/SummaryService.kt
@@ -4,17 +4,46 @@ import ms.homemonitor.shared.summary.repository.RepositoryWithTotals
 import ms.homemonitor.shared.summary.service.model.YearSummary
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Service
 class SummaryService {
     private val cache = mutableMapOf<RepositoryWithTotals, YearSummary>()
 
-    @Scheduled(cron = "0 59 * * * *")
+    @Scheduled(cron = "0 15 * * * *")
     private fun clearCache() {
         cache.clear()
     }
 
     fun getSummary(repositoryWithTotals: RepositoryWithTotals): YearSummary {
-        return cache.getOrPut(repositoryWithTotals) { YearSummary.ofHour(repositoryWithTotals)}
+        return cache.getOrPut(repositoryWithTotals) { yearSummaryUntilLastHour(repositoryWithTotals)}
+    }
+
+    private fun yearSummaryUntilLastHour(repository: RepositoryWithTotals): YearSummary {
+        val now = LocalDateTime.now()
+        val hoursInYear = 24 * if (now.toLocalDate().isLeapYear) 366 else 365
+        val thisYear = now.year
+        val prevYear = thisYear - 1
+
+        val yearStart = LocalDate.of(thisYear, 1, 1).atStartOfDay()
+        val todayHour = LocalDateTime.of(thisYear, 1, 1, now.hour, 0, 0)
+        val prevYearStart = LocalDate.of(prevYear, 1, 1).atStartOfDay()
+        val todayHourPrevYear= LocalDateTime.of(prevYear, now.month, now.dayOfMonth, now.hour, 0, 0)
+
+        val actualPrevYear = repository.getTotalBetweenDates(prevYearStart, yearStart)
+        val actualRunningYear = repository.getTotalBetweenDates(todayHourPrevYear, todayHour)
+        val actualYTDPrevYear = repository.getTotalBetweenDates(prevYearStart, todayHourPrevYear)
+        val actualYTD = repository.getTotalBetweenDates(yearStart, todayHour)
+
+        val hoursBetween = ChronoUnit.HOURS.between(yearStart, todayHour)
+        val yearExpectationExtrapolate = hoursInYear * actualYTD / hoursBetween
+        val yearExpectationComparedWithLastYear = (actualYTD / actualYTDPrevYear) * actualPrevYear
+
+        return YearSummary(
+            actualPrevYear, actualYTD, actualYTDPrevYear, actualRunningYear,
+            yearExpectationExtrapolate, yearExpectationComparedWithLastYear
+        )
     }
 }
--- src/main/kotlin/ms/homemonitor/shared/summary/service/model/YearSummary.kt
+++ src/main/kotlin/ms/homemonitor/shared/summary/service/model/YearSummary.kt
@@ -1,10 +1,5 @@
 package ms.homemonitor.shared.summary.service.model
 
-import ms.homemonitor.shared.summary.repository.RepositoryWithTotals
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
-
 data class YearSummary(
     val actualPreviousYear: Double,
     val actualYTD: Double,
@@ -12,58 +7,4 @@ data class YearSummary(
     val actualRunningYear: Double,
     val yearExpectationExtrapolate: Double,
     val yearExpectationComparedWithLastYear: Double) {
-
-    companion object {
-        fun of(repository: RepositoryWithTotals): YearSummary {
-            val today = LocalDate.now()
-            val thisYear = today.year
-            val yearStart = LocalDate.of(thisYear, 1, 1)
-
-            val prevYear = thisYear - 1
-            val prevYearStart = LocalDate.of(prevYear, 1, 1)
-            val todayPrevYear = LocalDate.of(prevYear, today.month, today.dayOfMonth)
-
-            val actualPrevYear = repository.getTotalBetweenDates(prevYearStart.atStartOfDay(), yearStart.atStartOfDay())
-            val actualRunningYear = repository.getTotalBetweenDates(todayPrevYear.atStartOfDay(), today.atStartOfDay())
-            val actualYTDPrevYear = repository.getTotalBetweenDates(prevYearStart.atStartOfDay(), todayPrevYear.atStartOfDay())
-            val actualYTD = repository.getTotalBetweenDates(yearStart.atStartOfDay(), today.atStartOfDay())
-
-
-            val daysBetween = ChronoUnit.DAYS.between(yearStart, today)
-            val daysInYear = if (LocalDate.now().isLeapYear) 366 else 365
-            val yearExpectationExtrapolate = daysInYear * actualYTD / daysBetween
-            val yearExpectationComparedWithLastYear = (actualYTD / actualYTDPrevYear) * actualPrevYear
-            return YearSummary(
-                actualPrevYear, actualYTD, actualYTDPrevYear, actualRunningYear,
-                yearExpectationExtrapolate, yearExpectationComparedWithLastYear
-            )
-        }
-
-        fun ofHour(repository: RepositoryWithTotals): YearSummary {
-            val now = LocalDateTime.now()
-            val thisYear = now.year
-            val yearStart = LocalDate.of(thisYear, 1, 1).atStartOfDay()
-            val todayHour = LocalDateTime.of(thisYear, 1, 1, now.hour, 0, 0)
-
-            val prevYear = thisYear - 1
-            val prevYearStart = LocalDate.of(prevYear, 1, 1).atStartOfDay()
-            val todayHourPrevYear= LocalDateTime.of(prevYear, now.month, now.dayOfMonth, now.hour, 0, 0)
-
-
-            val actualPrevYear = repository.getTotalBetweenDates(prevYearStart, yearStart)
-            val actualRunningYear = repository.getTotalBetweenDates(todayHourPrevYear, todayHour)
-            val actualYTDPrevYear = repository.getTotalBetweenDates(prevYearStart, todayHourPrevYear)
-            val actualYTD = repository.getTotalBetweenDates(yearStart, todayHour)
-
-            val hoursBetween = ChronoUnit.HOURS.between(yearStart, todayHour)
-            val hoursInYear = 24 * if (LocalDate.now().isLeapYear) 366 else 365
-            val yearExpectationExtrapolate = hoursInYear * actualYTD / hoursBetween
-            val yearExpectationComparedWithLastYear = (actualYTD / actualYTDPrevYear) * actualPrevYear
-            return YearSummary(
-                actualPrevYear, actualYTD, actualYTDPrevYear, actualRunningYear,
-                yearExpectationExtrapolate, yearExpectationComparedWithLastYear
-            )
-        }
-
-    }
 }
